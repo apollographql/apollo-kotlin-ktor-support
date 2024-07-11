8ce9a59cc629620dc1c4fc17dea1c8c52a58aa6e
martinbonnin <martin@mbonnin.net>
2024-07-11 19:34:35 +0000

    Deploying to gh-pages from @ apollographql/apollo-kotlin-ktor-support@2f1487bc9088608fe09ce083827f3b50250f8f56 🚀

--- apollo-engine-ktor/src/concurrentMain/kotlin/com/apollographql/ktor/http/KtorHttpEngine.concurrent.kt
+++
@@ -1,7 +0,0 @@
-package com.apollographql.ktor.http
-
-import io.ktor.client.plugins.*
-
-internal actual fun HttpTimeout.HttpTimeoutCapabilityConfiguration.setReadTimeout(readTimeoutMillis: Long) {
-  this.socketTimeoutMillis = readTimeoutMillis
-}
