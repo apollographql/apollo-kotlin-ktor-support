6348eae02672530855a0bf503f750d5cc66b6f56
sonatard <sonatard12@gmail.com>
2024-10-11 02:25:51 +0900

    Apollo4.1.0 and Ktor 3.0.0

--- apollo-engine-ktor/src/concurrentMain/kotlin/com/apollographql/ktor/http/KtorHttpEngine.concurrent.kt
+++ apollo-engine-ktor/src/concurrentMain/kotlin/com/apollographql/ktor/http/KtorHttpEngine.concurrent.kt
@@ -2,6 +2,6 @@ package com.apollographql.ktor.http
 
 import io.ktor.client.plugins.*
 
-internal actual fun HttpTimeout.HttpTimeoutCapabilityConfiguration.setReadTimeout(readTimeoutMillis: Long) {
+internal actual fun HttpTimeoutConfig.setReadTimeout(readTimeoutMillis: Long) {
   this.socketTimeoutMillis = readTimeoutMillis
 }
